68e5fa7df0c3814775f309a4ef7040159fc27733
und3fined <me@und3fined.com>
2023-12-15 00:57:33 +0700

    chore: add traits for txn

--- crates/core/src/pristine/prelude.rs
+++ crates/core/src/pristine/prelude.rs
@@ -1,5 +1,6 @@
 // Copyright (c) 2023 und3fy.dev. All rights reserved.
 // Created by und3fined <me@und3fy.dev> on 2023 Dec 14.
+#![allow(dead_code)]
 
 use std::sync::Arc;
 
--- crates/core/src/traits/mod.rs
+++ crates/core/src/traits/mod.rs
@@ -1,2 +1,8 @@
 // Copyright (c) 2023 und3fy.dev. All rights reserved.
 // Created by und3fined <me@und3fy.dev> on 2023 Dec 10.
+
+mod mut_txn;
+mod txn;
+
+pub use mut_txn::*;
+pub use txn::*;
